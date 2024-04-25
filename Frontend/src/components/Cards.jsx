043628a96fbab773c09ae59473bc227a2a4de03f
bhagyanimash <bhagyanimash2000@gmail.com>
2024-04-25 12:27:11 +0530

    Created landing page

---
+++ Frontend/src/components/Cards.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Cards({ item }) {
+  console.log(item);
+  return (
+    <>
+      <div className="mt-4 mb-4 p-3">
+        <div className="card w-92 bg-base-100 shadow-xl">
+          <figure>
+            <img src={item.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge bg-green-500 text-white">
+                {item.category}
+              </div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-green-500 hover:text-white duration-200">
+                Buy Now
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
