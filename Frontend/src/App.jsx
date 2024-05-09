b7e57dc0706207b2ae9d5be87404ee9226cc6da9
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-05-09 20:16:50 +0530

    first

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-      </Routes>
+      </Routes> 
     </>
   );
 }
